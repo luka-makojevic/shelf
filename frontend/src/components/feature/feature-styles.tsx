7c469260837e68405810ed97ca1d9dc61fc9328c
nemanja.pesic <nemanja.pesic@htecgroup.com>
2021-11-10 11:24:45 +0100

    fix eslint config, storybook init

--- frontend/src/components/feature/feature-styles.tsx
+++ frontend/src/components/feature/feature-styles.tsx
@@ -1,6 +1,15 @@
+import { ReactNode } from 'react'
 import styled from 'styled-components'
+import { space, SpaceProps, typography, TypographyProps } from 'styled-system'
+
+interface TitleProps extends TypographyProps, SpaceProps {
+  children: ReactNode
+}
 
 export const Container = styled.div``
-export const Title = styled.h1``
+export const Title = styled.h1<TitleProps>`
+  ${typography}
+  ${space}
+`
 export const Logo = styled.img``
 export const SubTitle = styled.p``
--- frontend/src/containers/HomeContainer.tsx
+++ frontend/src/containers/HomeContainer.tsx
@@ -1,9 +1,10 @@
 import { Feature } from '../components'
+import { Title } from '../components/feature/feature-styles'
 
 const HomeContainer = () => {
   return (
     <Feature>
-      <Feature.Title>Welcome to Shelf Storage Service</Feature.Title>
+      <Title fontSize={2}>Hello</Title>
       <Feature.SubTitle>
         Event-driven compute service that lets you run code for virtually any
         type of application or backend service without provisioning or managing
