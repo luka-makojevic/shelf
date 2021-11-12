d88b88aa1f4ec0e84d5f9f931435697cfdd8de60
stefanHTEC <stefan.bozovic@htecgroup.com>
2021-11-12 16:37:37 +0100

    CU-1rw1w17 Created SecurityConstants

--- backend/shelf-server/src/main/java/com/htec/shelfserver/security/WebSecurity.java
+++ backend/shelf-server/src/main/java/com/htec/shelfserver/security/WebSecurity.java
@@ -34,7 +34,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST , "/users")
+                .antMatchers(HttpMethod.POST , SecurityConstants.SIGN_UP_URL)
                 .permitAll()
                 .anyRequest()
                 .authenticated();
