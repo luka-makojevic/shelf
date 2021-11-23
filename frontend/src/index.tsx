1bd28a88ca655ed4c09fc3205aef175eb7797306
Goran Pantic <goran.pantic@htecgroup.com>
2021-11-23 14:12:14 +0100

    Cu 1td9674 http services new (#55)
    * CU-1td9679, resolved conflicts
    
    * CU-1td9674, code cleanup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { GlobalStyles } from './global-styles'
-// import { AuthProvider } from './providers/authProvider'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from './global-styles';
+import { AuthProvider } from './providers/authProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     <Router>
-      {/* <AuthProvider> */}
-      <App />
-      {/* </AuthProvider> */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
--- frontend/src/services/authServices.ts
+++ frontend/src/services/authServices.ts
@@ -1,19 +1,20 @@
-import axios from "axios"
+import axios from 'axios';
+import { RegisterData, LoginData } from '../interfaces/types';
 
-const API_URL = ""
+const API_URL = 'http://10.10.0.117:8080/users/';
 
+const register = (data: RegisterData) => axios.post(`${API_URL}register`, data);
 
-export const httpLogin = (data:any) => {
-    return axios
-      .post(`${API_URL}register`, {
-        data,
-      })
-      .then((res) => res)
-}
-export const httpRegister = (data:any) => {
-    return axios
-      .post(`${API_URL}register`, {
-        data,
-      })
-      .then((res) => res)
-}
+const login = (data: LoginData) => axios.post(`${API_URL}login`, data);
+
+const logout = (setUser: (arg: null) => void) => {
+  // missing features from backend
+  localStorage.removeItem('user');
+  setUser(null);
+};
+
+export default {
+  register,
+  login,
+  logout,
+};
