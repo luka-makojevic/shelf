925f6733cbf03e2cf5ae018a02039681c0e9e883
LidijaVM <93919682+LidijaVM@users.noreply.github.com>
2021-11-24 14:02:39 +0100

    CU-1u45xpc added_helper_for_restApi (#60)

---
+++ qa/src/test/java/helpers/RestHelpers.java
@@ -0,0 +1,71 @@
+package helpers;
+
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
+public class RestHelpers {
+
+    /**
+     * Method for sending Post request when request specification object is inserted as parameter
+     *
+     * @param reqSpec request specification object
+     * @return response which can be used for assertion
+     * @author lidija.veljkovic
+     *
+     */
+
+    public static Response sendPostRequest(RequestSpecification reqSpec){
+
+        return given().spec(reqSpec).log().all().relaxedHTTPSValidation().when()
+                .post().then().log().all().extract().response();
+    }
+
+    /**
+     * Method for sending Get request when request specification object is inserted as parameter
+     *
+     * @param reqSpec request specification object
+     * @return response which can be used for assertion
+     * @author lidija.veljkovic
+     *
+     */
+
+    public static Response sendGetRequest(RequestSpecification reqSpec){
+
+        return given().spec(reqSpec).log().all().relaxedHTTPSValidation().when()
+                .get().then().log().all().extract().response();
+    }
+
+    /**
+     * Method for sending Patch request when request specification object is inserted as parameter
+     *
+     * @param reqSpec request specification object
+     * @return response which can be used for assertion
+     * @author lidija.veljkovic
+     *
+     */
+
+    public static Response sendPatchRequest(RequestSpecification reqSpec){
+
+        return given().spec(reqSpec).log().all().relaxedHTTPSValidation().when()
+                .patch().then().log().all().extract().response();
+    }
+
+    /**
+     * Method for sending Delete request when request specification object is inserted as parameter
+     *
+     * @param reqSpec request specification object
+     * @return response which can be used for assertion
+     * @author lidija.veljkovic
+     *
+     */
+
+    public static Response sendDeleteRequest(RequestSpecification reqSpec){
+
+        return given().spec(reqSpec).log().all().relaxedHTTPSValidation().when()
+                .delete().then().log().all().extract().response();
+    }
+
+}
