7ce02ca70b700ad53513b5936898de3368438975
Sanja Nikolic <sanja.nikolic@htecgroup.com>
2021-11-25 10:14:29 +0100

    corrected the unresolved conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,5 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
-<<<<<<< HEAD
-import Landing from './pages/landing';
-import Login from './pages/login';
-import Register from './pages/register';
-import { Routes } from './enums/routes';
-import { RouteProps } from './interfaces/route';
-import TermsAndConditions from './pages/termsAndConditions';
-
-function App() {
-  const routes: RouteProps[] = [
-    {
-      path: Routes.HOME,
-      element: <Landing />,
-    },
-    {
-      path: Routes.REGISTER,
-      element: <Register />,
-    },
-    {
-      path: Routes.LOGIN,
-      element: <Login />,
-    },
-    {
-      path: Routes.DASHBOARD,
-      element: <Dashboard />,
-    },
-    {
-      path: Routes.TERMS,
-      element: <TermsAndConditions />,
-    },
-  ];
-  const routing = useRoutes(routes);
-
-  return <ThemeProvider theme={theme}>{routing}</ThemeProvider>;
-}
-=======
 import Router from './router';
 
 const App = () => (
@@ -43,6 +7,5 @@ const App = () => (
     <Router />
   </ThemeProvider>
 );
->>>>>>> master
 
 export default App;
