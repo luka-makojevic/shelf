f92a50c8926f3a8eb9aaf3c840acc7830440ee15
luka-makojevic <93662804+luka-makojevic@users.noreply.github.com>
2021-11-30 16:32:22 +0100

    Cu 1v0k9wt adding database migrations with flyway (#109)
    * CU-1v0k9wt - Flyway , added database autocreation , updated creation scipt
    
    * CU-1v0k9wt - Flyway , refactored migration files

--- backend/shelf-server/src/main/resources/db/migration/V1__createInitialTables.sql
+++ backend/shelf-server/src/main/resources/db/migration/V1__createInitialTables.sql
@@ -50,5 +50,3 @@ CREATE TABLE `email_verify_token` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
