5f52e60d11c6afdced077166105748216f848ffd
Lidija <lidija.veljkovic@htecgroup.com>
2021-12-06 17:47:53 +0100

    CU-1v6g3kh added_changes_on_login_page

--- qa/src/test/java/pages/LoginPage.java
+++ qa/src/test/java/pages/LoginPage.java
@@ -1,10 +1,9 @@
 package pages;
 
-        import org.openqa.selenium.By;
-        import org.openqa.selenium.WebDriver;
-        import org.openqa.selenium.WebElement;
-        import org.openqa.selenium.support.FindBy;
-        import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
@@ -15,29 +14,37 @@ public class LoginPage {
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy(name = "email") WebElement username;
+    @FindBy(name = "email") WebElement email;
     @FindBy(name = "password") WebElement password;
     @FindBy(css = "sc-bBHxTw kIUaGY") WebElement eyeIcon;
     @FindBy(css = "form button") WebElement signInButton;
     @FindBy(css = "[href='/register']") WebElement signUpButton;
+    @FindBy(linkText = "Reset password") WebElement resetPassword;
+    public @FindBy(xpath = "//*[contains(text(),'Invalid email format')]") WebElement invalidEmailMessage;
+    public @FindBy(xpath = "//*[contains(text(),'Password must have at least 8 characters')]") WebElement invalidPasswordMessage;
+    public @FindBy(xpath = "//*[@id=\"root\"]/div[2]/div[1]/div/form/div[2]/div[1]/div") WebElement blankEmailFieldMessage;
+    public @FindBy(xpath = "//*[@id=\"root\"]/div[2]/div[1]/div/form/div[2]/div[2]/div") WebElement blankPasswordFieldMessage;
 
+    /**
+     * @author lidija.veljkovic
+     */
     public void loginAsUser(String username, String password){
-        this.username.clear();
-        this.username.sendKeys(username);
+        this.email.clear();
+        this.email.sendKeys(username);
         this.password.clear();
         this.password.sendKeys(password);
         this.signInButton.click();
-
     }
-
     public void clickEyeIcon(){
 
         this.eyeIcon.click();
     }
-
     public void clickSignUpButton() {
 
         this.signUpButton.click();
+    }
+    public void clickResetPassword(){
+        this.resetPassword.click();
     }
 
 }
--- qa/src/test/java/tests/BaseTest.java
+++ qa/src/test/java/tests/BaseTest.java
@@ -9,9 +9,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.LoginPage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
-    public class BaseTest
+public class BaseTest
 {
     public WebDriver driver;
     public WebDriverWait wait;
@@ -19,6 +21,7 @@ import java.io.IOException;
     public BaseWdWaitHelpers baseWdWaitHelpers;
     public ExcelReader excelReader;
     public LoginPage loginPage;
+    public static Properties prop;
 
     @Before
     public void initialize() throws IOException
@@ -29,6 +32,26 @@ import java.io.IOException;
         driver.manage().window().maximize();
         excelReader = new ExcelReader("src/main/resources/ExcelRead.xlsx");
         loginPage = new LoginPage(driver);
+    }
+    /**
+     * @author stefan.gajic
+     */
+    public void navigateToPageUrl(String url) throws IOException {
+        prop = new Properties();
+        FileInputStream fis = new FileInputStream (".\\datafiles\\data.properties");
+        prop.load(fis);
+        String pageUrl = prop.getProperty(url);
+        driver.navigate().to(pageUrl);
+    }
+    /**
+     * @author stefan.gajic
+     */
+    public String getPageUrl(String url) throws IOException {
+        prop = new Properties();
+        FileInputStream fis = new FileInputStream (".\\datafiles\\data.properties");
+        prop.load(fis);
+        String pageUrl = prop.getProperty(url);
+        return pageUrl;
     }
 /*
     @After
