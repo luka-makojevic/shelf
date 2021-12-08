175a65dccc2f8265b58306dd42db600055614bf9
LidijaVM <93919682+LidijaVM@users.noreply.github.com>
2021-12-08 19:20:51 +0100

    CU-1v6g3kh added changes to login page (#123)
    * CU-1v6g3kh add changes to login page
    
    * CU-1v6g3kh added_changes_on_login_page
    
    * return changes on wd wait
    
    * CU-1v6g3kh added_tests_for_login_page

--- qa/src/test/java/tests/LoginTest.java
+++ qa/src/test/java/tests/LoginTest.java
@@ -1,38 +1,86 @@
 package tests;
-
-        import org.junit.Before;
-        import org.junit.Test;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.IOException;
 
 public class LoginTest extends BaseTest {
+
     @Before
-    public void pageSetUp() {
+    public void pageSetUp() throws IOException {
 
-        driver.navigate().to("http://localhost:3000/login");
+        navigateToPageUrl("loginPageUrl");
     }
-
     @Test
-    public void invalidUsername() throws InterruptedException {
+    public void userShouldBeLoggedIn() throws IOException {
 
-        for (int i = 1; i < excelReader.getLastRowNumberFromSheet("LoginTest"); i++) {
+        String email = excelReader.getStringData("LoginTest", 1,0);
+        String password = excelReader.getStringData("LoginTest", 1, 1);
+        loginPage.loginAsUser(email, password);
+        baseWdWaitHelpers.waitUrlToBe(getPageUrl("dashboardPageUrl"));
+        Assert.assertEquals(driver.getCurrentUrl(), getPageUrl("dashboardPageUrl"));
+    }
+    @Test
+    public void userShouldNotLoginWithInvalidEmailFormat(){
+      
+        for (int i = 1; i < excelReader.getLastRowNumberFromSheet("LoginTest") -2; i++) {
 
-            String username = excelReader.getStringData("LoginTest", i, 2);
+            String email = excelReader.getStringData("LoginTest", i, 2);
             String password = excelReader.getStringData("LoginTest", 1, 1);
-
-            loginPage.loginAsUser(username, password);
-
+            loginPage.loginAsUser(email, password);
+            String expectedMessage = "Authentication credentials not valid.";
+            String message = loginPage.invalidEmailMessage.getText();
+            Assert.assertTrue("Invalid email format", message.contains(expectedMessage));
         }
     }
-
     @Test
-    public void invalidPassword() throws InterruptedException {
+    public void userShouldNotLoginWithInvalidPassword() {
 
-        for (int i = 1; i < excelReader.getLastRowNumberFromSheet("LoginTest"); i++) {
-
+        for (int i = 1; i < excelReader.getLastRowNumberFromSheet("LoginTest") + 1; i++) {
+          
             String username = excelReader.getStringData("LoginTest", 1, 0);
             String password = excelReader.getStringData("LoginTest", i, 3);
-
             loginPage.loginAsUser(username, password);
-
         }
+        String expectedMessage = "Authentication credentials not valid.";
+        String message = loginPage.invalidPasswordMessage.getText();
+        Assert.assertTrue("Authentication credentials not valid.", message.contains(expectedMessage));
+    }
+    @Test
+    public void userShouldNotLoginWithBlankEmailField(){
+
+        String email = excelReader.getStringData("LoginTest", 3, 0);
+        String password = excelReader.getStringData("LoginTest", 1, 1);
+        loginPage.loginAsUser(email, password);
+        String expectedMessage = "This field is required";
+        String message = loginPage.blankEmailFieldMessage.getText();
+        Assert.assertTrue("This field is required", message.contains(expectedMessage));
+    }
+    @Test
+    public void userShouldNotLoginWithBlankPasswordField(){
+
+        String email = excelReader.getStringData("LoginTest", 1, 0);
+        String password = excelReader.getStringData("LoginTest", 2, 1);
+        loginPage.loginAsUser(email, password);
+        String expectedMessage = "This field is required";
+        String message = loginPage.blankPasswordFieldMessage.getText();
+        Assert.assertTrue("This field is required", message.contains(expectedMessage));
+    }
+    @Test
+    public void userShouldRedirectedToRegistrationPageWhenClicksSignUpButton() throws IOException {
+        loginPage.clickSignUpButton();
+        baseWdWaitHelpers.waitUrlToBe(getPageUrl("registrationPageUrl"));
+        Assert.assertEquals(driver.getCurrentUrl(), getPageUrl("registrationPageUrl"));
+    }
+    @Test
+    public void userShouldRedirectToForgotPasswordPage() throws IOException {
+        loginPage.clickResetPassword();
+        baseWdWaitHelpers.waitUrlToBe(getPageUrl("forgotPassword"));
+        Assert.assertEquals(driver.getCurrentUrl(), getPageUrl("forgotPassword"));
+    }
+    @After
+    public void deleteCookies(){
+        driver.manage().deleteAllCookies();
     }
 }
