30a564a29af273a93e2d4cf17f030902e4ec135c
Luka Makojevic <luka.makojevic@htecgroup.com>
2021-12-10 13:21:38 +0100

    CU-1wm4jug - Creating gateway microservice, added file request forwarding

--- backend/gateway/src/main/java/com/htec/gateway/service/GatewayService.java
+++ backend/gateway/src/main/java/com/htec/gateway/service/GatewayService.java
@@ -1,25 +1,31 @@
 package com.htec.gateway.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.*;
 
 @Service
 public class GatewayService {
 
-    private final String AUTH_MICROSERVICE_NAME = "account";
-    private final String AUTH_ENDPOINT_PATH = "/account/auth/authenticate";
+
+    private final static String AUTH_MICROSERVICE_NAME = "account";
+    private final static String AUTH_ENDPOINT_PATH = "/account/auth/authenticate";
+
+    public final static String SHELF_HEADER = "Shelf-Header";
+    public final static String FILE_REQUEST_SHELF_HEADER = "File-request";
 
     private final Map<String, String> apiServerUrls;
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     public GatewayService(@Value("#{${apiServers}}") Map<String, String> apiServerUrls,
                           RestTemplate restTemplate) {
@@ -27,24 +33,65 @@ public class GatewayService {
         this.restTemplate = restTemplate;
     }
 
-    public ResponseEntity<byte[]> sendRequest(RequestEntity<byte[]> entity) {
+    public ResponseEntity<byte[]> sendRequest(RequestEntity<byte[]> request, HttpServletRequest multipartRequest) throws IOException {
 
-        String microserviceName = getMicroserviceName(entity.getUrl().getPath());
+        String microserviceName = getMicroserviceName(request.getUrl().getPath());
 
         if (!AUTH_MICROSERVICE_NAME.equals(microserviceName)) {
 
             String accountApiUrl = getApiUrl(AUTH_MICROSERVICE_NAME, AUTH_ENDPOINT_PATH);
 
-            ResponseEntity<byte[]> authRet = send(accountApiUrl, HttpMethod.GET, new HttpEntity<>(entity.getHeaders()));
+            ResponseEntity<byte[]> authRet = send(accountApiUrl, HttpMethod.GET, new HttpEntity<>(request.getHeaders()));
 
             if (!authRet.getStatusCode().equals(HttpStatus.OK)) {
                 return authRet;
             }
         }
 
-        String apiUrl = getApiUrl(microserviceName, entity.getUrl().getPath());
+        String apiUrl = getApiUrl(microserviceName, request.getUrl().getPath());
 
-        return send(apiUrl, Objects.requireNonNull(entity.getMethod()), entity);
+        HttpEntity<byte[]> forwardingRequest = buildHttpEntity(request, multipartRequest);
+
+        return send(apiUrl, Objects.requireNonNull(request.getMethod()), forwardingRequest);
+    }
+
+    private HttpEntity<byte[]> buildHttpEntity(RequestEntity<byte[]> request, HttpServletRequest multipartRequest) throws IOException {
+
+        List<String> strings = request.getHeaders().get(SHELF_HEADER);
+
+        if (multipartRequest != null && strings != null && strings.get(0).contains(FILE_REQUEST_SHELF_HEADER)) {
+
+            Map<String, byte[]> map = new HashMap<>();
+
+            MultipartHttpServletRequest multipartReq = null;
+            if (multipartRequest instanceof MultipartHttpServletRequest) {
+                multipartReq = ((MultipartHttpServletRequest) multipartRequest);
+            } else {
+                return request;
+            }
+
+            Iterator<String> filesIterator = multipartReq.getFileNames();
+
+            while (filesIterator.hasNext()) {
+
+                String tempFileName = filesIterator.next();
+
+                MultipartFile file = multipartReq.getFile(tempFileName);
+
+                if (file != null && !file.isEmpty()) {
+                    map.put(tempFileName, file.getBytes());
+                }
+            }
+
+            HttpHeaders headers = new HttpHeaders(filterRequestHeaders(request.getHeaders()));
+
+            ObjectMapper mapper = new ObjectMapper();
+            byte[] bytes = mapper.writeValueAsBytes(map);
+
+            return new HttpEntity<>(bytes, headers);
+        }
+
+        return request;
     }
 
     private ResponseEntity<byte[]> send(String apiUrl, HttpMethod httpMethod, HttpEntity<byte[]> request) {
@@ -62,6 +109,22 @@ public class GatewayService {
                     .headers(filterResponseHeaders(ex.getResponseHeaders()))
                     .body(ex.getResponseBodyAsByteArray());
         }
+    }
+
+    private HttpHeaders filterRequestHeaders(HttpHeaders headers) {
+
+        HttpHeaders newHeaders = new HttpHeaders();
+
+        for (String headerName : headers.keySet()) {
+            if (HttpHeaders.CONTENT_TYPE.equals(headerName)) {
+                continue;
+            }
+            newHeaders.put(headerName, Collections.singletonList(headers.getFirst(headerName)));
+        }
+
+        newHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        return newHeaders;
     }
 
     private HttpHeaders filterResponseHeaders(HttpHeaders headers) {
