8f932641061569f13e21386c419349659ef249ff
shelfsrdjan <93597706+shelfsrdjan@users.noreply.github.com>
2021-12-13 02:25:42 -0800

    CU-1wu0tud code refactoring after reverting (#160)

--- qa/src/test/java/pages/User.java
+++ qa/src/test/java/pages/User.java
@@ -10,7 +10,8 @@ public class User
     public String firstName;
     public String lastName;
 
-    public User setValuesForValidUser(String email,String password, String firstName, String lastName) throws IOException
+    public User setValuesForValidUser(String email,String password, String firstName, String lastName)
+            throws IOException
     {
         ExcelReader excelReader = new ExcelReader("src/main/resources/ExcelRead.xlsx");
         this.email = email = excelReader.getStringData("apiTest",1,0);
@@ -19,14 +20,30 @@ public class User
         this.lastName = lastName = excelReader.getStringData("apiTest",1,3);
         return this;
     }
-
-    public User setValuesForInvalidUser(int i,String email, String password, String firstName, String lastName) throws IOException
+    public User setValuesForInvalidUser(int i,String email, String password, String firstName, String
+            lastName) throws IOException
     {
         ExcelReader excelReader = new ExcelReader("src/main/resources/ExcelRead.xlsx");
         this.email = email = excelReader.getStringData("apiTest", i,0);
         this.password = password = excelReader.getStringData("apiTest", i,1);
         this.firstName = firstName = excelReader.getStringData("apiTest", i,2);
         this.lastName = lastName = excelReader.getStringData("apiTest", i,3);
+        return this;
+    }
+    public User setValuesForValidUserToLogin(String email,String password) throws IOException
+    {
+        ExcelReader excelReader = new ExcelReader("src/main/resources/ExcelRead.xlsx");
+        this.email = email = excelReader.getStringData("apiTest",1,0);
+        this.password = password = excelReader.getStringData("apiTest",1,1);
+        return this;
+    }
+    public User setValuesForUpdatingUser(String firstName,String lastName, String password) throws
+            IOException
+    {
+        ExcelReader excelReader = new ExcelReader("src/main/resources/ExcelRead.xlsx");
+        this.firstName = firstName = excelReader.getStringData("apiTest",1,4);
+        this.lastName = lastName = excelReader.getStringData("apiTest",1,5);
+        this.password = password = excelReader.getStringData("apiTest",1,6);
         return this;
     }
 }
