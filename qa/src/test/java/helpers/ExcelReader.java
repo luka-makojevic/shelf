47ad60c1583c13803c992761c11bc2912749761d
LidijaVM <93919682+LidijaVM@users.noreply.github.com>
2021-12-13 13:39:29 +0100

    CU-1wm4gyw added_changes_on_suit_classes (#162)

--- qa/src/test/java/helpers/ExcelReader.java
+++ qa/src/test/java/helpers/ExcelReader.java
@@ -41,9 +41,40 @@ public class ExcelReader {
         return sheet.getLastRowNum();
     }
 
-    public int getLastRowNumberFromSheet(String sheetName)
-    {
+    public int getLastRowNumberFromSheet(String sheetName) {
         this.sheet = wb.getSheet(sheetName);
         return sheet.getLastRowNum();
     }
+
+
+    /** Method for getting the last row number from a specific column in Excel sheet
+     *
+     * @param  sheetName
+     * @param  columnName
+     * @return int
+     * @author stefan.gajic
+     */
+    public int getLastRowNumberFromColumn(String sheetName, String columnName) {
+        int rowNum = 0;
+        int colNum = 0;
+        this.sheet = wb.getSheet(sheetName);
+        this.row = sheet.getRow(rowNum);
+
+        // Selects desired column by its name
+        for (int i = 0; i < row.getLastCellNum(); i++) {
+            if (row.getCell(i).getStringCellValue().equals(columnName)) {
+                colNum = i;}
+        }
+        //Determines last row number from desired column
+        for(int i = 1; i <= sheet.getLastRowNum(); i++) {
+            row = sheet.getRow(i);
+            if (row.getCell(colNum).getStringCellValue().equals("")) {
+                rowNum = i-1;
+                break;
+            } else {
+                rowNum = sheet.getLastRowNum();}
+        }
+        return rowNum;
+    }
+
 }
