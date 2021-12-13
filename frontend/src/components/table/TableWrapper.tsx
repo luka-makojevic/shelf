00c212ed0b0129adbc4e7abaa23a0321fa94b267
sanja-htec <93662696+sanja-htec@users.noreply.github.com>
2021-12-13 15:37:07 +0100

    CU-1wm6gfh_Table-Wrapper_Sanja-Nikolic, table wrapper component made (#156)

---
+++ frontend/src/components/table/TableWrapper.tsx
@@ -0,0 +1,22 @@
+import {
+  TableContainer,
+  TableContent,
+  TableDescription,
+  TableTitle,
+} from './tableWrapper-styles';
+
+export interface TableWrapperProps {
+  title: string;
+  description?: string;
+  children: JSX.Element | JSX.Element[];
+}
+
+const TableWrapper = ({ title, description, children }: TableWrapperProps) => (
+  <TableContainer>
+    <TableTitle>{title}</TableTitle>
+    <TableDescription>{description}</TableDescription>
+    <TableContent>{children}</TableContent>
+  </TableContainer>
+);
+
+export default TableWrapper;
