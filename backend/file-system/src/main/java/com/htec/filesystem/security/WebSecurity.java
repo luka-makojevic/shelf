76b2a00a99898915e675fea26345463c7b693744
Damnjan Askovic <damnjan.askovic@htecgroup.com>
2021-12-14 10:06:09 +0100

    CU-1xgnjet - Swagger for File System

---
+++ backend/file-system/src/main/java/com/htec/filesystem/security/WebSecurity.java
@@ -0,0 +1,26 @@
+package com.htec.filesystem.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(
+                        "/v2/api-docs",
+                        "/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        "/swagger.json")
+                .permitAll();
+    }
+}
