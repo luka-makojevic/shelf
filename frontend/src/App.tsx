f4ce8dc6279b321cca80bc0a0793b0b2983c4bf1
pavlovic-nemanja <nemanja.pavlovic@htecgroup.com>
2021-12-14 12:26:18 +0100

    minor changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import Router from './components/router';
-import { Header } from './components';
-import { Table } from './components/table/table';
 
 const App = () => (
-  <>
-    <Header />
-    <Table mulitSelect />
-  </>
+  <ThemeProvider theme={theme}>
+    <Router />
+  </ThemeProvider>
 );
 
 export default App;
