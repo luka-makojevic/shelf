4ef5fb10fe769c895fb2b5abb0c490f60a330aab
luka-makojevic <93662804+luka-makojevic@users.noreply.github.com>
2021-12-15 12:49:01 +0100

    CU-1xpfbwg - Refactoring folder initialization (#177)
    * CU-1xpfbwg - Refactoring folder initialization
    
    * CU-1xpfbwg - Refactoring folder initialization, FileDTO added

--- backend/file-system/src/main/java/com/htec/filesystem/controller/FileController.java
+++ backend/file-system/src/main/java/com/htec/filesystem/controller/FileController.java
@@ -3,7 +3,6 @@ package com.htec.filesystem.controller;
 import com.htec.filesystem.model.response.FileResponseModel;
 import com.htec.filesystem.model.response.TextResponseMessage;
 import com.htec.filesystem.service.FileService;
-import com.htec.filesystem.service.UserAPICallService;
 import com.htec.filesystem.util.FileUtil;
 import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
@@ -47,18 +46,6 @@ public class FileController {
 
         FileResponseModel fileResponseModel = fileService.getFile(FileUtil.getFilePath(request.getUrl().getPath()));
         return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(fileResponseModel.getImageContent());
-    }
-
-    @PostMapping("/initialize/{id}")
-    public ResponseEntity initializeFolders(@PathVariable Long id) {
-
-        HttpStatus retStatus = HttpStatus.OK;
-
-        if (!fileService.initializeFolders(id)) {
-            retStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
-
-        return ResponseEntity.status(retStatus).body(new TextResponseMessage("Folders created", retStatus.value()));
     }
 
     @PostMapping("/upload/{shelfId}/{folderId}")
--- backend/file-system/src/main/java/com/htec/filesystem/controller/FolderController.java
+++ backend/file-system/src/main/java/com/htec/filesystem/controller/FolderController.java
@@ -1,7 +1,11 @@
 package com.htec.filesystem.controller;
 
-import com.htec.filesystem.service.FileService;
+import com.htec.filesystem.model.response.TextResponseMessage;
 import com.htec.filesystem.service.FolderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,18 @@ public class FolderController {
 
     public FolderController(FolderService folderService) {
         this.folderService = folderService;
+    }
+
+    @PostMapping("/initialize/{id}")
+    public ResponseEntity initializeFolders(@PathVariable Long id) {
+
+        HttpStatus retStatus = HttpStatus.OK;
+
+        if (!folderService.initializeFolders(id)) {
+            retStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return ResponseEntity.status(retStatus).body(new TextResponseMessage("Folders created", retStatus.value()));
     }
 
 }
