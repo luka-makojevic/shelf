c5f4e9ebdc0e1a4b360df7e9ecc7b45be73502d1
Luka Makojevic <luka.makojevic@htecgroup.com>
2021-12-15 12:46:00 +0100

    CU-1xpfbwg - Refactoring folder initialization, FileDTO added

--- backend/file-system/src/main/java/com/htec/filesystem/dto/FileDTO.java
+++ backend/file-system/src/main/java/com/htec/filesystem/dto/FileDTO.java
@@ -1,4 +1,28 @@
 package com.htec.filesystem.dto;
 
-public class FileDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileDTO {
+    private Long id;
+    private String name;
+    private String path;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime deletedAt;
+    private LocalDateTime updatedAt;
+
+    private boolean isDeleted;
+    private boolean isFolder;
+
+    private Long shelfId;
+    private Long parentFolderId;
 }
