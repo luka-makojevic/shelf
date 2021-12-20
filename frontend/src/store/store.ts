44ca408004b16274d9da84100867d493d848f5a2
pavlovic-nemanja <nemanja.pavlovic@htecgroup.com>
2021-12-20 13:28:46 +0100

    CU-1y21bbu added store for pathHistory and data for file/folders page

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import loadingReducer from './loadingReducer';
 import userReducer from './userReducer';
 import shelfReducer from './shelfReducer';
+import fileReducer from './fileReducer';
+import pathHistoryReducer from './pathHistory';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     loading: loadingReducer,
     shelf: shelfReducer,
+    file: fileReducer,
+    pathHistory: pathHistoryReducer,
   },
 });
 export type AppDispatch = typeof store.dispatch;
