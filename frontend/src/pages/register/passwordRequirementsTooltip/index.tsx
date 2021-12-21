ca9bcfb397e8f641bd335d6d72c3945ce29b17ee
nemanja-pesic <93662629+nemanja-pesic@users.noreply.github.com>
2021-12-21 09:19:07 +0100

    add password requirements tooltip (#206)

---
+++ frontend/src/pages/register/passwordRequirementsTooltip/index.tsx
@@ -0,0 +1,22 @@
+import {
+  ListItem,
+  List,
+  Text,
+  TooltipWrapper,
+} from './passwordRequirementsTooltip-styles';
+
+const PasswordRequirementsTooltip = () => (
+  <TooltipWrapper>
+    <Text>
+      Passwords must contain at least 8 characters and contain the following:
+    </Text>
+    <List>
+      <ListItem>Uppercase letter</ListItem>
+      <ListItem>Lowercase letter</ListItem>
+      <ListItem>Number</ListItem>
+      <ListItem>Special character</ListItem>
+    </List>
+  </TooltipWrapper>
+);
+
+export default PasswordRequirementsTooltip;
