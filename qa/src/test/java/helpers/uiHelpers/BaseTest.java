14e477462e12966536e5f42638004089d5c54163
Lidija <lidija.veljkovic@htecgroup.com>
2021-12-30 12:58:15 +0100

    CU-1y7q8d7 added_elements_and_tests_for_dashboard_page

--- qa/src/test/java/helpers/uiHelpers/BaseTest.java
+++ qa/src/test/java/helpers/uiHelpers/BaseTest.java
@@ -1,12 +1,16 @@
 package helpers.uiHelpers;
 
 import helpers.excelHelpers.ExcelReader;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
+import pages.DashboardPage;
 import pages.LoginPage;
 import pages.LoginViaMicrosoft;
 import pages.RegistrationPage;
+import tests.frontendTests.LoginTest;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -21,11 +25,13 @@ public class BaseTest
     public static NavigateBrowserHelper navigateBrowser;
     public static ExcelReader excelReader;
     public static LoginPage loginPage;
+    public static LoginTest loginTest;
     public static LoginViaMicrosoft loginViaMicrosoft;
     public static RegistrationPage regPage;
+    public static DashboardPage dashboardPage;
 
-    @BeforeClass
-    public static void initialize() throws IOException
+    @Before
+    public void initialize() throws IOException
     {
         webDriverManager = new WebDriverManager();
         driver = webDriverManager.initializeDriver();
@@ -33,13 +39,15 @@ public class BaseTest
         navigateBrowser = new NavigateBrowserHelper(driver, prop, wdWaitHelpers);
         driver.manage().window().maximize();
         excelReader = new ExcelReader("src/main/resources/ExcelRead.xlsx");
-        loginPage = new LoginPage(driver);
+        loginPage = new LoginPage(driver, wdWaitHelpers, excelReader, navigateBrowser);
+        loginTest = new LoginTest();
         loginViaMicrosoft = new LoginViaMicrosoft(driver, wdWaitHelpers);
         regPage = new RegistrationPage(driver, wdWaitHelpers);
+        dashboardPage = new DashboardPage(driver, wdWaitHelpers);
     }
 
-    @AfterClass
-    public static void testTearDown() {
+    @After
+    public void testTearDown() {
         driver.close();
         driver.quit();
     }
--- qa/src/test/java/pages/LoginPage.java
+++ qa/src/test/java/pages/LoginPage.java
@@ -1,19 +1,30 @@
 package pages;
 
+import helpers.excelHelpers.ExcelReader;
+import helpers.uiHelpers.NavigateBrowserHelper;
+import helpers.uiHelpers.WdWaitHelpers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.io.IOException;
+
 public class LoginPage {
 
     WebDriver driver;
+    WdWaitHelpers wdWaitHelpers;
+    ExcelReader excelReader;
+    NavigateBrowserHelper navigateBrowser;
 
-    public LoginPage(WebDriver driver) {
+    public LoginPage(WebDriver driver, WdWaitHelpers wdWaitHelpers, ExcelReader excelReader,
+                     NavigateBrowserHelper navigateBrowser) {
         this.driver = driver;
+        this.wdWaitHelpers = wdWaitHelpers;
+        this.excelReader = excelReader;
+        this.navigateBrowser = navigateBrowser;
         PageFactory.initElements(driver, this);
     }
-
     @FindBy(name = "email") WebElement email;
     @FindBy(name = "password") WebElement password;
     @FindBy(css = "form button") WebElement signInButton;
@@ -33,8 +44,18 @@ public class LoginPage {
         this.password.sendKeys(password);
         this.signInButton.click();
     }
+    /**
+     * Method for loading valid data from Excel for filling out loggIn form
+     *
+     * @author lidija.veljkovic
+     */
+    public void successfulLoggIn() throws IOException {
+        String email = excelReader.getStringData("LoginTest", 2,0);
+        String password = excelReader.getStringData("LoginTest", 2, 1);
+        loginAsUser(email, password);
+        wdWaitHelpers.waitUrlToBe(navigateBrowser.getPageUrl("dashboardPageUrl"));
+    }
     public void clickSignUpButton() {
-
         this.signUpButton.click();
     }
     public void clickResetPassword(){
