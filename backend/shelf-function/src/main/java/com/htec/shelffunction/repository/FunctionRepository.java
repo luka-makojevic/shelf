3b6eba9401f9c44f1679c5186a0b76c33998b2b2
Luka Makojevic <luka.makojevic@htecgroup.com>
2022-01-14 12:49:22 +0100

    CU-22b9a9v - Get user's shelf functions - removed unused imports

--- backend/shelf-function/src/main/java/com/htec/shelffunction/repository/FunctionRepository.java
+++ backend/shelf-function/src/main/java/com/htec/shelffunction/repository/FunctionRepository.java
@@ -2,10 +2,6 @@ package com.htec.shelffunction.repository;
 
 import com.htec.shelffunction.entity.FunctionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 import java.util.List;
 
