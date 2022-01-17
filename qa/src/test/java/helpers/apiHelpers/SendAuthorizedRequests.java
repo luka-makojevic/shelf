343e987ba6d507e76f426fbbabdc1ee30f23d6fc
shelfsrdjan <srdjan.rados@htecgroup.com>
2022-01-17 17:02:22 +0100

    CU-1y7qb5e delete unnecessary commented code

--- qa/src/test/java/helpers/apiHelpers/SendAuthorizedRequests.java
+++ qa/src/test/java/helpers/apiHelpers/SendAuthorizedRequests.java
@@ -5,14 +5,11 @@ import helpers.propertieHelpers.PropertieManager;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import models.FileSys;
 
 import java.io.File;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static helpers.apiHelpers.BaseApiTest.*;
 
@@ -177,22 +174,6 @@ public class SendAuthorizedRequests
         return tokenGenerated;
     }
 
-//    public Response setShelf(String shelfTitle, String tokenGenerated)
-//    {
-//        String shelfName = responseToJson.setShelfName(shelfTitle);
-//        String parsedJson = gson.toJson(shelfName);
-//        Response response = sendingPostReqForCreateShelf(tokenGenerated,parsedJson);
-//        return response;
-//    }
-
-//    public Integer getShelfId(String tokenGenerated)
-//    {
-//        Response response = sendAuthorizedRequests.sendingGetShelfReq(tokenGenerated);
-//        ArrayList<Integer> shelfIdArray = response.jsonPath().get("id");
-//        Integer shelfId = shelfIdArray.get(0);
-//        return shelfId;
-//    }
-
     public Response sendingPutReqWithGeneratedTokenFLCShelf(String tokenGenerated, Integer shelfId)
     {
         RequestSpecBuilder builder = new RequestSpecBuilder();
@@ -256,33 +237,4 @@ public class SendAuthorizedRequests
         Response response = RestHelpers.sendPutRequest(reqSpec);
         return response;
     }
-
-//    public FileSys setFolder(String tokenGenerated, String folderTitle)
-//    {
-//        Response response = sendAuthorizedRequests.sendingGetShelfReq(tokenGenerated);
-//        ArrayList<Integer> shelfIdArray = response.jsonPath().get("id");
-//        Integer shelfId = shelfIdArray.get(0);
-//        fileSys.setValues(folderTitle,0, shelfId);
-//        return fileSys;
-//    }
-
-//    public List<Long> createFolderAndMoveItToTrash(String tokenGenerated)
-//    {
-//        sendAuthorizedRequests.setShelf("Shelfara123",tokenGenerated);
-//        sendAuthorizedRequests.setFolder(tokenGenerated,"Fascikla");
-//        String parsedJson = gson.toJson(fileSys);
-//
-//        sendAuthorizedRequests.sendingPostReqForCreateFolder(tokenGenerated,parsedJson);
-//        Response response = sendAuthorizedRequests.sendingPutReqWithGeneratedTokenFLCShelf(tokenGenerated,fileSys.shelfId);
-//
-//        List<Map<String,Object>> shelfItems = responseToJson.setShelfItems(response.jsonPath().get("shelfItems"));
-//        Integer folderId = (Integer) shelfItems.get(0).get("id");
-//
-//        List<Long> list = new ArrayList<>();
-//        list.add(Long.valueOf(folderId));
-//
-//        sendAuthorizedRequests.sendingPutReqToMoveFolderToTrash(tokenGenerated, list);
-//        return list;
-//    }
-
 }
