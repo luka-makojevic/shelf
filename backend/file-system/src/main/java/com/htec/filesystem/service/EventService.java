8bc677d0c666aacf7e138b9b11182b10b1a7a68b
Luka Makojevic <luka.makojevic@htecgroup.com>
2022-01-19 15:40:19 +0100

    CU-2321qrp - Send download event to Kafka topic

--- backend/file-system/src/main/java/com/htec/filesystem/service/EventService.java
+++ backend/file-system/src/main/java/com/htec/filesystem/service/EventService.java
@@ -1,6 +1,8 @@
 package com.htec.filesystem.service;
 
+import com.htec.filesystem.exception.ExceptionSupplier;
 import com.htec.filesystem.model.request.KafkaRequestModel;
+import com.htec.filesystem.repository.FileRepository;
 import com.htec.filesystem.util.FunctionEvents;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -14,15 +16,22 @@ public class EventService {
     private final String TOPIC_NAME = "event";
 
     private final FunctionService functionService;
+    private final FileRepository fileRepository;
 
     @Autowired
     private KafkaTemplate<String, KafkaRequestModel> kafkaTemplate;
 
-    public EventService(FunctionService functionService) {
+    public EventService(FunctionService functionService,
+                        FileRepository fileRepository) {
         this.functionService = functionService;
+        this.fileRepository = fileRepository;
     }
 
     public void reportEvent(FunctionEvents event, List<Long> fileIds, Long userId, Long shelfId) {
+
+        if(shelfId == null && !fileIds.isEmpty()){
+            shelfId = fileRepository.findById(fileIds.get(0)).orElseThrow(ExceptionSupplier.fileNotFound).getShelfId();
+        }
 
         List<Long> functionToBeExecutedIds = functionService.getUserFunctionsByShelfId(shelfId, event.getValue());
 
