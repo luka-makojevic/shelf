9b4a95c78007aab9c45dadb79dadcdb549dd1b66
damnjan-askovic <93923146+damnjan-askovic@users.noreply.github.com>
2022-01-21 11:30:08 +0100

    CU-2381q6j - [Bug] User can create shelf if he enters only a space -> fixed (#317)

--- backend/file-system/src/main/java/com/htec/filesystem/validator/FileSystemValidator.java
+++ backend/file-system/src/main/java/com/htec/filesystem/validator/FileSystemValidator.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class FileSystemValidator {
 
-    private static final String SHELF_NAME_PATTERN = "^.{1,50}$";
+    private static final String SHELF_NAME_PATTERN = "^[A-Za-z _]*[A-Za-z0-9][A-Za-z _!@#$%^&-_+]{1,50}$";
 
     private static final Pattern pattern = Pattern.compile(SHELF_NAME_PATTERN);
 
